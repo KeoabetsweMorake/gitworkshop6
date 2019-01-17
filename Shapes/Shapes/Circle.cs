05b1d2b4c435c80c475d085e4baca7c5c57bc672
Joseph-LeGrand <jLegrand@retrorabbit.co.za>
2019-01-17 10:46:55 +0200

    Added circle area feature

---
+++ Shapes/Shapes/Circle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    class Circle
+    {
+        private const double PI = 22 / 7;
+
+        public double radius { get; set; }
+        public void Area(double radius)
+        {
+            System.Console.WriteLine(PI * (radius * radius));
+        }
+    }
+}
