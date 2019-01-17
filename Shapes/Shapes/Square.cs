9af0319d35a059501c4489db94533ef5b2b294d4
KeoabetsweMorake <kmorake@retrorabbit.co.za>
2019-01-17 10:42:54 +0200

    Adding the Square class, with area calculation

---
+++ Shapes/Shapes/Square.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    class Square
+    {
+        public double Lenght { get; set; }
+        public double Width { get; set; }
+        public double Area { get; set; }
+
+        public Square()
+        {
+
+        }
+
+        public Square(double length, double width)
+        {
+            Lenght = length;
+            Width = width;
+        }
+
+        public void CalcArea()
+        {
+            Area = Lenght * Width;
+        }
+    }
+}
